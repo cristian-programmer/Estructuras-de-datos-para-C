e6817ea6c3e6148566d48cee1141b7cad024d958
Cristian vargas gonzalez <cvg97@misena.edu.co>
2018-12-15 00:23:15 -0500

    si se utiliza el operador & en un condicional devolvera si hay un 1 en la operacion

--- binary_operations/andoperation.c
+++ binary_operations/andoperation.c
@@ -17,11 +17,17 @@
     si se utiliza el operador & en un condicional devolvera si hay un 1 en la operacion 
 */
 int main(){
-    char a = 23;
-    char b = 90;
+    char a = 75;
+    char b = 8;
     char resultado;
 
     printf("a = %d b= %d ",a, b);
-    resultado = a & b;
+    // resultado = a & b;
+    if( a & b) {
+        printf("%s", "se ha encontrado un valor 1 en la variable b");
+
+    }else{
+        printf("%s", "no hay ningun 1 solo 0 ");
+    }
     printf(" resultado = %d", resultado);
 }
